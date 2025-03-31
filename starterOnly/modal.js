a54b1f5f15ab55f6342da1a8b9afef76130ca4da
Mathieu FENOUIL <matfen2.0@outlook.fr>
2025-03-31 15:54:44 +0200

    Ajout des dernières modifications
    Affichage des messages réussites et d'erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,53 +9,22 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalConfirm = document.getElementById("confirmationModal");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-// Sélectionne tous les éléments avec la classe "js-close" (dans ce cas, la croix de fermeture dans le modal)
 const closeBtns = document.querySelectorAll(".js-close");
-
-// Evénement de la fermeture du modal
-// Pour chaque bouton de fermeture trouvé (même s'il n'y en a qu'un ici)
-closeBtns.forEach((b) =>
-  b.addEventListener("click", () => {
-    // Quand on clique sur la croix, on appelle la fonction closeModal()
-    closeModal();
-  })
-);
-
-// Fonction pour fermer le modal
-function closeModal() {
-  // Cache le modal principal en mettant son style display à "none"
-  modalbg.style.display = "none";
-  
-  // Cache également le modal de confirmation (même s'il n'est pas visible dans le code HTML fourni)
-  // Cette ligne est probablement là pour gérer un cas futur où un modal de confirmation serait affiché
-  modalConfirm.style.display = "none";
-}
-
-// Evenement du formulaire de réservation
-// Form Ligne 60
+const navLink = document.querySelector("#nav");
 const form = document.querySelector('form[name="reserve"]');
 
-// Ajout d'un écouteur d'évènement "submit" sur le formulaire
+// ajout d'un écouteur d'évènement "submit" sur le formulaire
 form.addEventListener("submit", function (event) {
-  // Empeche le formulaire de se soumettre
   event.preventDefault();
 
   // Initialisation du tableau des erreurs
   const errors = [];
 
-  // Sélectionner tous les élements du formulaire
+  // vérification du prénom
   const prenomElement = document.getElementById("first");
-  const nomElement = document.getElementById("last");
-  const emailElement = document.getElementById("email");
-  const dateNaissanceElement = document.getElementById("birthdate");
-  const nbConcoursElement = document.getElementById("quantity");
-  const listBtnRadiovilleElement = document.getElementById("choixVille");
-  const accepterConditionsElement = document.getElementById("checkbox1");
-
-  // Vérification du prénom
   const prenom = prenomElement.value;
   const validPrenom = validerPrenom(prenom);
   if (validPrenom === false) {
@@ -69,6 +38,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification du nom
+  const nomElement = document.getElementById("last");
   const nom = nomElement.value;
   const validNom = validerNom(nom);
   if (validNom === false) {
@@ -82,6 +52,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification de l'email
+  const emailElement = document.getElementById("email");
   const email = emailElement.value;
   const validEmail = validerEmail(email);
   if (validEmail === false) {
@@ -92,6 +63,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification de champ date de naisance non vide & âge du participant supérieur à 18 ans
+  const dateNaissanceElement = document.getElementById("birthdate");
   dateNaiss = new Date(dateNaissanceElement.value);
   const validDateNaiss18 = valider18ans(dateNaiss);
 
@@ -116,6 +88,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification du nombre de concours auxquels le user a déjà participé saisi par l'utilisateur
+  const nbConcoursElement = document.getElementById("quantity");
   const nbConcours = nbConcoursElement.value;
   const validNbConcours = validerNbConcours(nbConcours);
   if (validNbConcours === false) {
@@ -129,6 +102,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification des boutons radios pour s'assurer de la sélection d'une ville
+  const listBtnRadiovilleElement = document.getElementById("choixVille");
   if (validerBtnRadioVille() === false) {
     errors.push({
       fieldName: "ville",
@@ -140,6 +114,7 @@ form.addEventListener("submit", function (event) {
   }
 
   // vérification de la case à cocher pour confirmer l'acceptation des conditions d'utilisation
+  const accepterConditionsElement = document.getElementById("checkbox1");
   if (validerAccepterConditions() === false) {
     errors.push({
       fieldName: "conditions d'utilisation",
@@ -166,40 +141,80 @@ form.addEventListener("submit", function (event) {
   }
 });
 
+function setErrorElement(input) {
+  const errorEl = input.closest(".formData");
+  errorEl.setAttribute("data-error-visible", "true");
+}
+
+function resetErrorElement(input) {
+  const errorEl = input.closest(".formData");
+  errorEl.removeAttribute("data-error-visible");
+}
+
+navLink.addEventListener("click", editNav);
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// launch modal formConfirm
+function launchModalConfirm() {
+  modalConfirm.style.display = "block";
+}
+
+// close modal event
+closeBtns.forEach((b) =>
+  b.addEventListener("click", () => {
+    // Quand on a cliqué sur la croix, on ferme la popup
+    closeModal();
+  })
+);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+  modalConfirm.style.display = "none";
+}
+
+
 /**
  * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
  * ici : deux caractères au minimum
  * @param {string} prenom
  */
 function validerPrenom(prenom) {
- if (prenom.length >= 2) {
-   return true;
- }
- return false;
+  if (prenom.length >= 2) {
+    return true;
+  }
+  return false;
 }
 
 /**
-* Cette fonction prend un nom en paramètre et valide qu'il est au bon format
-* ici : deux caractères au minimum
-* @param {string} nom
-*/
+ * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
+ * ici : deux caractères au minimum
+ * @param {string} nom
+ */
 function validerNom(nom) {
- if (nom.length >= 2) {
-   return true;
- }
- return false;
+  if (nom.length >= 2) {
+    return true;
+  }
+  return false;
 }
 
 /**
-* Cette fonction prend un email en paramètre et valide qu'il est au bon format.
-* @param {string} email
-*/
+ * Cette fonction prend un email en paramètre et valide qu'il est au bon format.
+ * @param {string} email
+ */
 function validerEmail(email) {
- let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
- if (!emailRegExp.test(email)) {
-   return false;
- }
- return true;
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!emailRegExp.test(email)) {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -282,24 +297,3 @@ function validerPrchEvenmnt() {
   let PrchEvenmnt = balisePrchEvenmnt.checked;
   console.log(PrchEvenmnt); // affiche true ou false dans la console mais pourrait déclencher l'envoi d'un mail dans une version plus évoluée du projet
 }
-
-
-function setErrorElement(input) {
-  const errorEl = input.closest(".formData");
-  errorEl.setAttribute("data-error-visible", "true");
-}
-
-function resetErrorElement(input) {
-  const errorEl = input.closest(".formData");
-  errorEl.removeAttribute("data-error-visible");
-}
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
